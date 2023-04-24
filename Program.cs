fd46adb7c10c5d27fb100ee929814e0cffb0e972
Laurits Bentsen <laurits2@hotmail.com>
2023-04-24 12:57:48 +0200

    Serializer now added and working. Doesn’t save answerslist as class member

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 ﻿namespace QuizMaker;
+using System.Xml.Serialization;
 class Program
 {
     static void Main(string[] args)
     {
+        XmlSerializer writer = new XmlSerializer(typeof(List<Question>));
+        var QuestionsList = new List<Question>();
         UIMethods.WelcomeMessage();
 
         while (Console.ReadKey(true).Key != ConsoleKey.D2)
         {
-            var question1 = new Question();
+            var Question = new Question();
+            QuestionsList.Add(Question);
             UIMethods.PromptForSaveAndExit();
         }
+        var path = "/Users/Cecilie/Projects/QuizMaker/QuizData.xml";
+        using (FileStream file = File.Create(path))
+        {
+            writer.Serialize(file, QuestionsList);
+        }
 
-        
+
     }
 }
 
