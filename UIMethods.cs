9bdba09c85a250420f9a991a3a6630e0a85b8cd7
Laurits Bentsen <laurits2@hotmail.com>
2023-04-24 09:19:36 +0200

    UI method for displaying welcome message added

--- UIMethods.cs
+++ UIMethods.cs
@@ -3,8 +3,10 @@ namespace QuizMaker
 {
     public class UIMethods
     {
-        public UIMethods()
+        public static void WelcomeMessage()
         {
+            Console.WriteLine("Welcome user! This is a quiz maker program. You can either save new questions or run program with saved questions," +
+                " if no prior questions has been saved, no output will be given");
         }
     }
 }
